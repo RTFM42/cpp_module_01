598add5d526c21f3607dc1caddce8df410aa7cee
yush <yus-sato@kalyte.ro>
2024-12-11 04:04:06 +0000

    update

---
+++ ex05/main.cpp
@@ -0,0 +1,13 @@
+#include "Harl.hpp"
+
+int main(void)
+{
+	Harl harl;
+
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("LEVLE");
+	return (0);
+}
