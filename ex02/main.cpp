75bd1ed0e92a77671cb0174c7cf8b14723c686ed
yush <yus-sato@kalyte.ro>
2024-12-11 02:33:36 +0000

    update

---
+++ ex02/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(void)
+{
+	std::string string;
+
+	string = "HT THIS IS BRAIN";
+
+	std::string *stringPTR = &string;
+	std::string &stringREF = string;
+
+	std::cout << "string address: " << &string << std::endl;
+	std::cout << "stringPTR address: " << stringPTR << std::endl;
+	std::cout << "stringREF address: " << &stringREF << std::endl;
+	std::cout << "stringPTR string: " << *stringPTR << std::endl;
+	std::cout << "stringREF string: " << stringREF << std::endl;
+}
