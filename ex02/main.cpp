1d6117505a711cc5e20d8f72e2208f6b0a24be62
yush <yus-sato@kalyte.ro>
2024-12-11 02:34:35 +0000

    update

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -9,7 +9,7 @@ int main(void)
 	std::string *stringPTR = &string;
 	std::string &stringREF = string;
 
-	std::cout << "string address: " << &string << std::endl;
+	std::cout << "string    address: " << &string << std::endl;
 	std::cout << "stringPTR address: " << stringPTR << std::endl;
 	std::cout << "stringREF address: " << &stringREF << std::endl;
 	std::cout << "stringPTR string: " << *stringPTR << std::endl;
